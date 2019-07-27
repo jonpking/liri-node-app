04ab6af214fd627eedb8ce188031093105e926a8
Enigma <jonpking@gmail.com>
2019-07-26 21:04:05 -0700

    put code for each switch case into its own function

--- liri.js
+++ liri.js
@@ -15,45 +15,17 @@ switch (apiSelector) {
 
     // Bands in Town api search
     case "concert-this":
-        axios.get("https://rest.bandsintown.com/artists/" + searchTerm + "/events?app_id=codingbootcamp")
-            .then(function (response) {
-                console.log("Venue Name: " + response.data[0].venue.name);
-                console.log("Venue City: " + response.data[0].venue.city);
-                console.log("Venue Region/State: " + response.data[0].venue.region);
-                console.log("Venue Country: " + response.data[0].venue.country);
-                console.log("Venue Date: " + moment(response.data[0].datetime).format("MM/DD/YYYY"));
-            });
+        concertThis(searchTerm);
         break;
 
     // Spotify api search
     case "spotify-this-song":
-        spotify.search({ type: "track", query: searchTerm, limit: 1 }, function (err, data) {
-            if (err) {
-                return console.log("Error occured: " + err);
-            }
-            console.log("Track Name: " + data.tracks.items[0].name);
-            console.log("Album Name: " + data.tracks.items[0].album.name);
-            console.log("Artist(s): " + data.tracks.items[0].artists[0].name);
-            console.log("Preview URL: " + data.tracks.items[0].preview_url);
-        });
+        spotifyThisSong(searchTerm);
         break;
 
     // OMDB api search
     case "movie-this":
-        if (!searchTerm) {
-            searchTerm = "Mr. Nobody"
-        }
-        axios.get("http://www.omdbapi.com/?apikey=trilogy&t=" + searchTerm)
-            .then(function (response) {
-                console.log(`Movie Title: ${response.data.Title}`);
-                console.log(`Release Year: ${response.data.Year}`);
-                console.log(`IMDB Rating: ${response.data.imdbRating}`);
-                console.log(`Rotten Tomatoes Rating: ${response.data.Ratings[1].Value}`);
-                console.log(`Country Produced: ${response.data.Country}`);
-                console.log(`Language: ${response.data.Language}`);
-                console.log(`Plot: ${response.data.Plot}`);
-                console.log(`Actors: ${response.data.Actors}`);
-            });
+        movieThis(searchTerm);
         break;
 
     case "do-what-it-says":
@@ -74,4 +46,44 @@ switch (apiSelector) {
             }
         });
         break;
+};
+
+function concertThis(searchTerm) {
+    axios.get("https://rest.bandsintown.com/artists/" + searchTerm + "/events?app_id=codingbootcamp")
+        .then(function (response) {
+            console.log("Venue Name: " + response.data[0].venue.name);
+            console.log("Venue City: " + response.data[0].venue.city);
+            console.log("Venue Region/State: " + response.data[0].venue.region);
+            console.log("Venue Country: " + response.data[0].venue.country);
+            console.log("Venue Date: " + moment(response.data[0].datetime).format("MM/DD/YYYY"));
+        });
+};
+
+function spotifyThisSong(searchTerm) {
+    spotify.search({ type: "track", query: searchTerm, limit: 1 }, function (err, data) {
+        if (err) {
+            return console.log("Error occured: " + err);
+        }
+        console.log("Track Name: " + data.tracks.items[0].name);
+        console.log("Album Name: " + data.tracks.items[0].album.name);
+        console.log("Artist(s): " + data.tracks.items[0].artists[0].name);
+        console.log("Preview URL: " + data.tracks.items[0].preview_url);
+    });
+};
+
+function movieThis(searchTerm) {
+    if (!searchTerm) {
+        searchTerm = "Mr. Nobody"
+    }
+    axios.get("http://www.omdbapi.com/?apikey=trilogy&t=" + searchTerm)
+        .then(function (response) {
+            console.log(`Movie Title: ${response.data.Title}`);
+            console.log(`Release Year: ${response.data.Year}`);
+            console.log(`IMDB Rating: ${response.data.imdbRating}`);
+            console.log(`Rotten Tomatoes Rating: ${response.data.Ratings[1].Value}`);
+            console.log(`Country Produced: ${response.data.Country}`);
+            console.log(`Language: ${response.data.Language}`);
+            console.log(`Plot: ${response.data.Plot}`);
+            console.log(`Actors: ${response.data.Actors}`);
+        });
 };
